02f40c96b678f9ca63bd7c9a95e7bf2ff5098705
jzolotar <zolotarjakub@gmail.com>
2022-07-31 21:47:35 +0200

    add redux store, global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { GlobalStyle } from './global/cssReset/GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <GlobalStyle />
+    <App />
+  </Provider>
+);
