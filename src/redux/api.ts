3fa750561a0645fd066a407f4dd961a3a29a4860
jzolotar <zolotarjakub@gmail.com>
2022-08-02 21:37:54 +0200

    add more logic to rtk query, 2 queries added, one for default data, second one for data based on user input

--- src/redux/api.ts
+++ src/redux/api.ts
@@ -1,7 +1,42 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+interface CharacterData {
+  url: string;
+  name: string;
+  culture: string;
+  born: string;
+  died: string;
+  titles: string[];
+  aliases: string[];
+  father: string;
+  mother: string;
+  spuse: string;
+  allegiances: string[];
+  books: string[];
+  povBooks: string[];
+  tvSeries: string[];
+  playedBy: string[];
+}
+
+interface InputType {
+  page: string;
+  pageSize: string;
+}
+
 export const gotApi = createApi({
   reducerPath: 'gotApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'dsdasdas' }),
-  endpoints: (builder) => ({}),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://www.anapioficeandfire.com/api/characters',
+  }),
+  endpoints: (builder) => ({
+    getCharactersByDefault: builder.query<CharacterData[], void>({
+      query: () => `?page=1&pageSize=10`,
+    }),
+    getCharactersByInput: builder.query<CharacterData[], InputType>({
+      query: ({ page, pageSize }) => `?page=${page}&pageSize=${pageSize}`,
+    }),
+  }),
 });
+
+export const { useGetCharactersByDefaultQuery, useGetCharactersByInputQuery } =
+  gotApi;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,24 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { gotApi } from './api';
 
-interface CharacterData {
-  url: string;
-  name: string;
-  culture: string;
-  born: string;
-  died: string;
-  titles: string[];
-  aliases: string[];
-  father: string;
-  mother: string;
-  spuse: string;
-  allegiances: string[];
-  books: string[];
-  povBooks: string[];
-  tvSeries: string[];
-  playedBy: string[];
-}
-
 export const store = configureStore({
   reducer: {
     [gotApi.reducerPath]: gotApi.reducer,
