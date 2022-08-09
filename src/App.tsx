047dc9d80e7ce1617f34770a06cd1386e8425c35
jzolotar <zolotarjakub@gmail.com>
2022-08-09 21:58:35 +0200

    mutate response data to show additional data regarding first/last/next pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import CharacterList from './components/characterList/CharacterList';
 import { useGetCharactersByDefaultQuery } from './redux/api';
 
 function App() {
-  const { data } = useGetCharactersByDefaultQuery();
+  const { data, isSuccess, isFetching } = useGetCharactersByDefaultQuery();
+
+  console.log('isFetching: ' + isFetching);
+  console.log('isSucces: ' + isSuccess);
+
+  if (isSuccess) {
+    console.log(data);
+  }
+
   return (
     <div>
       <CharacterList />
--- src/redux/api.ts
+++ src/redux/api.ts
@@ -1,5 +1,46 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+function parseLinkHeader(header: string): KeyValueMap<Object> {
+  let parts: string[];
+  let links: KeyValueMap<Object> = {};
+
+  if (header.length === 0) {
+    throw new Error('input must not be of zero length');
+  }
+
+  parts = header.split(',');
+  // Parse each part into a named link
+  parts.forEach((part) => {
+    let section: string[] = part.split(';');
+    let name: string;
+    let page: string;
+    let pageSize: string;
+
+    if (section.length !== 2) {
+      throw new Error("section could not be split on ';'");
+    }
+
+    const splitData = section[0].split('&');
+    page = splitData[0].trim().slice(55);
+    pageSize = splitData[1].slice(9).replace('>', '');
+
+    name = section[1].replace(/rel="(.*)"/, '$1').trim();
+    // links[name] = url;
+    const obj = {
+      name,
+      page,
+      pageSize,
+    };
+    links[name] = obj;
+  });
+
+  return links;
+}
+
+interface KeyValueMap<T> {
+  [key: string]: T;
+}
+
 interface CharacterData {
   url: string;
   name: string;
@@ -29,12 +70,20 @@ export const gotApi = createApi({
     baseUrl: 'https://www.anapioficeandfire.com/api/characters',
   }),
   endpoints: (builder) => ({
-    getCharactersByDefault: builder.query<CharacterData[], void>({
+    getCharactersByDefault: builder.query<
+      CharacterData[] | KeyValueMap<Object>,
+      void
+    >({
       query: () => `?page=1&pageSize=10`,
       transformResponse: (response: CharacterData[], meta, arg) => {
-        console.log(meta?.response?.headers.get('Link'));
-        console.log(Object.keys(meta!.response!.headers));
-        console.log(arg);
+        // console.log(meta?.response?.headers.get('Link'));
+        // console.log(Object.keys(meta!.response!.headers));
+        const link = meta!.response!.headers.get('Link');
+        if (link !== null) {
+          const testVal = { pageInfo: parseLinkHeader(link), data: response };
+          console.log(testVal);
+          return { pageInfo: parseLinkHeader(link), characters: response };
+        }
         return response;
       },
     }),
