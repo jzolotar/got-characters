a5989dc08ebe7b97ff92b26919b9b1d12e0ca9c2
jzolotar <zolotarjakub@gmail.com>
2022-08-27 10:27:27 +0200

    add nav btn to get back to characters page

--- src/pages/HousePage.tsx
+++ src/pages/HousePage.tsx
@@ -3,6 +3,7 @@ import Header from '../components/header/header';
 import House from '../components/house/House';
 import Loader from '../components/loader/Loader';
 import { useGetHouseDataQuery } from '../redux/api';
+import styled from 'styled-components';
 
 const HousePage = () => {
   let { id } = useParams();
@@ -15,6 +16,7 @@ const HousePage = () => {
   let content = (
     <>
       <Header title='House Page' />
+      <StyledBtn to='/characters'>back to characters list </StyledBtn>
       {isSuccess ? (
         <House
           name={data.name}
@@ -36,3 +38,24 @@ const HousePage = () => {
 };
 
 export default HousePage;
+
+const StyledBtn = styled(NavLink)`
+  text-transform: uppercase;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, 0);
+  top: 95px;
+  border-radius: 0.5rem;
+  border: none;
+  padding: 0.1rem 3.5rem;
+  text-decoration: none;
+  color: #000;
+  font-weight: 700;
+  border: 2px solid #333;
+  cursor: pointer;
+  @media (max-width: 833px) {
+    width: 320px;
+    text-align: center;
+    padding: 0.1rem 0;
+  }
+`;
